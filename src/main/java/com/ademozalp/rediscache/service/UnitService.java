c3b7dac264309761080c16a253b69496a83ded00
adem <ozalp.adem@ogr.ahievran.edu.tr>
2024-05-12 13:13:55 +0300

    Serializable sorunu çözüldü.

--- src/main/java/com/ademozalp/rediscache/service/UnitService.java
+++ src/main/java/com/ademozalp/rediscache/service/UnitService.java
@@ -12,7 +12,6 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
